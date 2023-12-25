a2bfc03563f671ff75b1744d21c31e75506baed5
Pavan kumar <pavankumargarapati04@gmail.com>
2023-12-25 19:27:41 +0530

    A new update

--- js/script.js
+++ js/script.js
@@ -1,39 +1,76 @@
-
 var navlist = document.getElementById('navitems');
-// var mainbd = document.querySelector('.main-background');
 function openmenu() {
     navlist.style.left = '0';
 }
 function closemenu() {
     navlist.style.left = '-200px';
 }
-
 document.querySelector("#menuopen").addEventListener("click", openmenu);
 
-$(".carousel").owlCarousel({
-    margin: 10,
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 2000,
-    autoplayHoverPause: true,
-    responsive: {
-        0: {
-            items: 1,
-            nav: false
-        },
-        500: {
-            items: 2,
-            nav: false
-        },
-        800: {
-            items: 3,
-            nav: false
-        },
-        1200: {
-            items: 4,
-            nav: false
+/* ----- keeps the links active ------*/
+
+document.addEventListener('DOMContentLoaded', function () {
+    
+    var currentUrl = window.location.href.split('#')[0];
+    var navLinks = document.querySelectorAll('.nav-links');
+
+    navLinks.forEach(function (link) {
+
+        var linkUrl = link.href.split('#')[0];
+
+        if (linkUrl === currentUrl) {
+            link.classList.add('active');
         }
-    }
+    });
 });
 
+/* -------- move to top or bottom accordingly -------- */
 
+document.addEventListener('DOMContentLoaded', function () {
+    
+    var backToTopLink = document.getElementById('backtotop');
+
+    window.addEventListener('scroll', function () {
+        if (this.oldScroll > this.scrollY) {
+            backToTopLink.href = '#top'; 
+            backToTopLink.querySelector('i').className = 'fa fa-arrow-up';
+        } else {
+            backToTopLink.href = '#bottom'; 
+            backToTopLink.querySelector('i').className = 'fa fa-arrow-down';    
+        }
+
+        this.oldScroll = this.scrollY;
+    });
+});
+
+/* ------ contact from --------- */
+
+function validateForm() {
+    var name = document.getElementById('username');
+    var phone = document.getElementById('phone');
+    var message = document.getElementById('message');
+
+    if (name.value.trim() === '' || phone.value.trim() === '' || message.value.trim() === '') {
+        showError('All fields are required.');
+        return false;
+    }
+
+    var phoneNoPattern = /^\d{10}$/;
+    if (!phoneNoPattern.test(phone.value)) {
+        showError('Please enter a valid phone number');
+        return false;
+    }
+
+    return true;
+}
+
+
+function showError(message) {
+    var errorMessage = document.getElementById('errorMessage');
+    errorMessage.textContent = message;
+}
+
+function success() {
+    var errorMessage = document.getElementById('errorMessage');
+    errorMessage.textContent = '';
+}
